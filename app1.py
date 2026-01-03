62572f365cccab86dcf49feba6764bce70ccd5a3
sougata-r <sougataroy272@gmail.com>
2026-01-03 20:47:32 +0530

    Add files via upload

--- app1.py
+++ app1.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
 import pandas as pd
 import pickle
+from tensorflow.keras.models import load_model
+
 
 # load the trained model
 with open('ohe_geo.pkl','rb') as file:
@@ -33,7 +35,7 @@ has_cr_card=st.selectbox('Has Credit Card',[0,1])
 is_active_member=st.selectbox('Is Active Memner',[0,1])
 
 # prepare input data
-input_data=({
+input_data=pd.DataFrame({
     'CreditScore':[credit_score],
     'Gender':[label_encoder_gender.transform([(gender)])[0]],
     'Age':[age],
@@ -46,16 +48,26 @@ input_data=({
 
 })
 
-# geography col is missing
-geo_encoded=ohe_geo.transform([[geography]]).toarray()
-geo_encoded_df=pd.DataFrame(geo_encoded,columns=ohe_geo.get_feature_names_out)
+# # geography col is missing
+# geo_encoded=ohe_geo.transform([[geography]]).toarray()
+# geo_encoded_df=pd.DataFrame(geo_encoded,columns=ohe_geo.get_feature_names_out(['Geography']))
+
+# One-hot encode geography (FOR SINGLE INPUT)
+geo_encoded = ohe_geo.transform(pd.DataFrame({'Geography': [geography]}))
+
+geo_encoded_df = pd.DataFrame(
+    geo_encoded.toarray(),
+    columns=ohe_geo.get_feature_names_out(['Geography'])
+)
+
 
 # concat
-input_data=pd.concat([input_data.reset_index(drop=True),geo_encoded_df],axis=1)
+input_data=pd.concat([input_data,geo_encoded_df],axis=1)
 
 # scaled
 input_data_scaled=scalar.transform(input_data)
 
+model=load_model('model.h5')
 # prediction churn
 prediction=model.predict(input_data_scaled)
 prediction_proba=prediction[0][0]
